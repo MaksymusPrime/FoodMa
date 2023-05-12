496163c4582562b40c90471979f3795b9ccb0fdc
MaksymusPrime <grafdemaxo@gmail.com>
2023-05-13 00:58:17 +0300

    reviewSec

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let navbar = document.querySelector('.navbar');
     }
 
 
-    let swiper = new Swiper(".home-slider", {
+    var swiper = new Swiper(".home-slider", {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
@@ -35,3 +35,27 @@ let navbar = document.querySelector('.navbar');
         },
       loop:true,
       });
+
+      var swiper = new Swiper(".review-slider", {
+        spaceBetween: 20,
+        centeredSlides: true,
+               autoplay: {
+          delay: 4500,
+          disableOnInteraction: false
+        },
+        loop:true,
+      breakpoints: {
+        0:{
+          slidesPerView: 1,
+        },
+        640:{
+          slidesPerView: 2,
+        },
+        768:{
+          slidesPerView: 2,
+        },
+        1024:{
+          slidesPerView: 3,
+        },
+      },
+            });
